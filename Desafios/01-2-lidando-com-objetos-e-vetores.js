8973513cafc8fc3d158ff9c5970319caf5b8a10d
Gabriel Marques <ti.gabrielmarques@protonmail.com>
2020-06-15 11:51:11 -0300

    Finishing second challenge

--- Desafios/01-2-lidando-com-objetos-e-vetores.js
+++ Desafios/01-2-lidando-com-objetos-e-vetores.js
@@ -6,6 +6,19 @@ const empresa = {
         rua: "Rua Guilherme Gembala",
         numero: 260
     }
-}
+};
 
 console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereço.rua}, ${empresa.endereço.numero}`);
+
+const programadores = [
+    {
+        nome: "Gabriel",
+        idade: 23,
+        tecnologias: [{
+            nome: "Java",
+            especialidade: "Back-end"
+        }]
+    }
+];
+
+console.log(`O usuário ${programadores[0].nome} tem ${programadores[0].idade} e usa a tecnologia ${programadores[0].tecnologias[0].nome} com especialidade em ${programadores[0].tecnologias[0].especialidade}`);
